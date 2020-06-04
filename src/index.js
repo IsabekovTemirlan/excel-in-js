5fa97cd231ef67fa449d653a83d14ebed39f35ea
IsabekovTemirlan <isabekovtemirlan@gmail.com>
2020-06-04 07:51:01 +0300

    Finish base

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-import './scss/index.scss'
-console.log('Working!!!')
+import './scss/index.scss';
+import {Excel} from '@/components/excel/Excel';
+import {Header} from "@/components/header/Header";
+import {Formula} from "@/components/formula/Formula";
+import {Table} from "@/components/table/Table";
+import {Toolbar} from "@/components/toolbar/Toolbar";
+
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table]
+});
+
+excel.render();
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,31 @@
-const path = require('path')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const isProd = process.env.NODE_ENV === 'production'
-const isDev = !isProd
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
 
-const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
+const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`;
 
 const jsLoaders = () => {
   const loaders = [
     {
       loader: 'babel-loader',
       options: {
-        presets: ['@babel/preset-env']
+        presets: ['@babel/preset-env'],
+        plugins: ["@babel/plugin-proposal-class-properties"]
       }
     }
-  ]
+  ];
 
   if (isDev) {
-    loaders.push('eslint-loader')
+    loaders.push('eslint-loader');
   }
 
-  return loaders
-}
+  return loaders;
+};
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
@@ -88,4 +89,4 @@ module.exports = {
       }
     ]
   }
-}
+};
