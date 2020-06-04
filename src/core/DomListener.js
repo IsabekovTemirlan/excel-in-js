4a6edbc9129d9c0876da3d0b1b9ea4b290d1435d
Temirlan <36995733+IsabekovTemirlan@users.noreply.github.com>
2020-06-04 11:00:04 +0600

    add all files

---
+++ src/core/DomListener.js
@@ -0,0 +1,38 @@
+import {capitalize} from "@core/utils";
+
+export class DomListener {
+  constructor($root, listeners = []) {
+    if (!$root) {
+      throw new Error('No $root provided for DOMListener');
+    }
+    this.$root = $root;
+    this.listeners = listeners;
+  }
+
+  initDOMListener() {
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener);
+      if (!this[method]) {
+        const name = this.name || '';
+        throw new Error(
+            `Method ${method} is not implemented in ${name} Component`
+        );
+      }
+      this[method] = this[method].bind(this);
+      this.$root.on(listener, this[method]);
+    });
+  }
+
+  removeDOMListeners() {
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener);
+      this.$root.off(listener, this[method]);
+    });
+  }
+}
+
+// input => OnInput
+
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName);
+}
